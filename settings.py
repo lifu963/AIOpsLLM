f07de180de5c7f99367b5c37210eb8977769da99
lifu963 <290023318@qq.com>
2024-10-08 21:37:18 +0800

    first commit

--- settings.py
+++ settings.py
@@ -1,5 +1,5 @@
 DEFAULT_WORKSPACE: str = 'workspace'
-DASHSCOPE_API_KEY = 'sk-a7320ba8c75b4326ac2b35009ad6d0c0'
+DASHSCOPE_API_KEY = '***'
 QWEN_URL = 'https://dashscope.aliyuncs.com/compatible-mode/v1'
 # MODEL = "qwen-turbo"
 MODEL = "qwen-plus"
